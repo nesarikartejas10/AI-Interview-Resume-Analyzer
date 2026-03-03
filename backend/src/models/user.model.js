e0a3d43c4540f7c968a3be2f9e9eda92b866e39d
nesarikartejas10 <nesarikartejas10@gmail.com>
2026-03-03 10:19:15 +0530

    create user model

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      unique: [true, "username already taken"],
+      required: true,
+    },
+
+    email: {
+      type: String,
+      unique: [true, "Account already exists with this email address"],
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+export const User = mongoose.model("User", userSchema);
