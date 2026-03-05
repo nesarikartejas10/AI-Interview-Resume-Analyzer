906bff64ee1131f618686135b10ab138f2f07ada
nesarikartejas10 <nesarikartejas10@gmail.com>
2026-03-05 10:53:33 +0530

    implement authentication context and wrap app with AuthProvider

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,13 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routing/app.routes";
+import { AuthProvider } from "./features/auth/auth.context";
 
 const App = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+  );
 };
 
 export default App;
