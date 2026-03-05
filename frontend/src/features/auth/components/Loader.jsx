5656b753689469558ddd7bffe1747be85c3ea59e
nesarikartejas10 <nesarikartejas10@gmail.com>
2026-03-05 12:52:39 +0530

    add loading indicator

---
+++ frontend/src/features/auth/components/Loader.jsx
@@ -0,0 +1,12 @@
+import "../../../styles/Loader.scss";
+
+const Loader = () => {
+  return (
+    <div className="lds-ripple">
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Loader;
