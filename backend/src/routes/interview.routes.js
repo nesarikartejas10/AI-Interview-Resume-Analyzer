c988c02a672a7b3ca9d0c66d1758a57416cf4c16
nesarikartejas10 <nesarikartejas10@gmail.com>
2026-03-07 08:41:10 +0530

    feat(interview): integrate multer middleware for resume upload

--- backend/src/routes/interview.routes.js
+++ backend/src/routes/interview.routes.js
@@ -1,9 +1,15 @@
 import express from "express";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
 import { generateInterviewReportController } from "../controllers/interview.controller.js";
+import { upload } from "../middlewares/file.middleware.js";
 
 const router = express.Router();
 
-router.post("/", authMiddleware, generateInterviewReportController);
+router.post(
+  "/",
+  authMiddleware,
+  upload.single("resume"),
+  generateInterviewReportController,
+);
 
 export default router;
