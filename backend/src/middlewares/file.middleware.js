c0b174cb99441fa5e29a1e9f641b2cbf84cd95a8
nesarikartejas10 <nesarikartejas10@gmail.com>
2026-03-07 10:36:53 +0530

    fix file upload middleware

--- backend/src/middlewares/file.middleware.js
+++ backend/src/middlewares/file.middleware.js
@@ -1,9 +1,8 @@
 import multer from "multer";
 
-export const upload = () =>
-  multer({
-    storage: multer.memoryStorage(),
-    limits: {
-      fileSize: 3 * 1024 * 1024, //3MB
-    },
-  });
+export const upload = multer({
+  storage: multer.memoryStorage(),
+  limits: {
+    fileSize: 3 * 1024 * 1024, //3MB
+  },
+});
